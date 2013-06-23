8b6b598e246430eaaca9cb97455490172ff9a869
realslimkarthik <karthikhariharan13@gmail.com>
2013-06-23 16:50:10 +0000

    CONNECT and GET requests handled

--- server.c
+++ server.c
@@ -7,17 +7,17 @@
 #include<netdb.h>
 #include<string.h>
 
-void convert_addr(char* buff);
-int tcpsock;
-int conn;
+struct sockaddr_in* convert_addr(char* buff);
+
 int main(int argc, char *argv[]) {
+	int tcpsock, conn;
 	tcpsock = socket(AF_INET, SOCK_STREAM, 0);
 	if(tcpsock < 0) {
 		perror("Socket Error");
 		exit(0);
 	}
 	
-	char buff[1024]; //for packet data
+	char buff[1024], resp[1024]; //for packet data
 
 	//usual server bindings
 	struct sockaddr_in prox_serv, cli, serv;
@@ -42,40 +42,55 @@ int main(int argc, char *argv[]) {
 	read(conn, (char *)buff, sizeof(buff));
 	printf("%s\n", buff);
 	/* Unsure about convert_addr() function signature and return type */
-	convert_addr(buff);
 
+	struct sockaddr_in* ipv4 = convert_addr(buff);
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	connect(sock, (struct sockaddr *)&ipv4, sizeof(struct sockaddr));
+	printf("ello\n");
+	write(sock, (char*)buff, sizeof(buff));
+	printf("written\n");
+	read(sock, (char*)resp, sizeof(resp));
+	printf("%s\n", resp);
 	close(conn);
 	close(tcpsock);
 	return 0;
 }
 
 
-void convert_addr(char* buff) {
+struct sockaddr_in* convert_addr(char* buff) {
+	char test[8];
 	char *host_name;
 	char *host;
-	host_name = strtok(buff, " ");
-	host_name = strtok(NULL, " ");
-	host = strtok(host_name, "/");
-	host = strtok(NULL, "/");
-	
-	printf("host:%s\n", host);
-	printf("%d\n", sizeof((char*)&host_name));
+	strncpy(test, buff, 7);
+	test[7] = '\0';
+	if(!strcmp(test, "CONNECT")) {
+		host_name = strtok(buff, " ");
+		host_name = strtok(NULL, " ");
+		char *host1 = strstr(host_name, ":");
+		strncpy(host, host_name, (int)host1 - (int)host_name);
+		printf("HHost:%s\n", host);
+	} else {
+		host_name = strtok(buff, "\n");
+		printf("host%s\n", host_name);
+		host_name = strtok(NULL, "\n");
+		host = strstr(host_name, " ");
+		(int)host++;
+		printf("HHost:%s\n", host);
+	}
 	struct addrinfo hints, *res;
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	int status;
 
-	status = getaddrinfo(host, "80", &hints, &res);
+	status = getaddrinfo(host, NULL, &hints, &res);
 	if(status!=0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
 		exit(2);
 	}
-	//connect using res.ai_addr
 	struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
-	
 	char ipstr[128];
-
 	inet_ntop(AF_INET, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
 	printf("%s\n", ipstr);
+	return (struct sockaddr_in*)res->ai_addr;
 }
